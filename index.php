44537343092be51b97d41ff7eb9d85ff9cfb12bf
KrinaPatel <krinapatel4793@gmail.com>
2019-05-29 14:58:14 +1200

    Create index.php
    index/home page design...

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+  include('header.php');
+?>
+  <body>
+ 
+    <!-- Top banner -->
+    <section class = "top-banner background-top-center">
+      <div class = "top-banner-overlay"></div>
+        <div class = "container">
+          <div class =  "row">
+            <div class = "col-md-12 col-sm-12 col-xs-12">
+              <div class = "top-banner-content">
+                <h1>Welcome to SafeTrade!</h1>
+                <h3>Multiple Jobs <br/>with multiple field – <b>available!</b></h3>
+                <a href = "#" class = "btn btn-default bg-darkorange text-white">Apply Now!</a>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
+
+    <!-- team-section-1 -->
+    <section class = "section-wrap team-content-1 clearfix">
+      <div class = "container">
+        <div class = "row team-member text-center">
+          <div class = "col-md-6 col-sm-6 col-xs-12">
+            <img class = "border-radius border-width border-box" src = "images/demo.jpg">
+            <h3>Ami Patel</h3>
+            <p>Developer</p>
+          </div>
+          <div class = "col-md-6 col-sm-6 col-xs-12">
+            <img class = "border-radius border-width border-box" src = "images/demo1.jpg">
+            <h3>Krina Patel</h3>
+            <p>Developer</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- team-section-1 end -->
+
+
+    <!-- Footer -->
+    <?php
+  		include('footer.php');
+  	?>
+    <!-- footer end-->
+
+    <!-- Bootstrap core JavaScript -->
+    <script src = "js/jquery-1.12.2.min.js"></script>
+    <script src = "js/main.js"></script>
+  </body>
+</html>
