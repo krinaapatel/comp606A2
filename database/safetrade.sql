101b6da650a6f62ca020b7ffd28b7c06300089d2
KrinaPatel <krinapatel4793@gmail.com>
2019-06-19 18:20:34 +1200

    Update safetrade.sql
    add sql database ....

--- database/safetrade.sql
+++ database/safetrade.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2019 at 03:00 PM
+-- Generation Time: Jun 19, 2019 at 07:59 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -25,157 +25,183 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `estimate`
+-- Table structure for table `bids`
 --
 
-CREATE TABLE `estimate` (
-  `id` int(11) NOT NULL,
-  `job_id` int(11) NOT NULL,
-  `job_posted_by` int(11) NOT NULL,
-  `trader_id` int(11) NOT NULL,
-  `cost_total` decimal(15,2) NOT NULL,
-  `cost_labour` decimal(15,2) NOT NULL,
-  `cost_material` decimal(15,2) NOT NULL,
-  `expiration_date` date NOT NULL
+CREATE TABLE `bids` (
+  `bid_id` int(11) NOT NULL,
+  `requierment_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `tdr_id` int(11) NOT NULL,
+  `bidt_amount` float(15,2) NOT NULL,
+  `bidl_amount` float(15,2) NOT NULL,
+  `bidm_amount` float(15,2) NOT NULL,
+  `time_arrival` date NOT NULL,
+  `createdon` date NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `estimate`
+-- Dumping data for table `bids`
 --
 
-INSERT INTO `estimate` (`id`, `job_id`, `job_posted_by`, `trader_id`, `cost_total`, `cost_labour`, `cost_material`, `expiration_date`) VALUES
-(11, 13, 19, 20, '1000.00', '400.00', '400.00', '2019-06-18');
+INSERT INTO `bids` (`bid_id`, `requierment_id`, `member_id`, `tdr_id`, `bidt_amount`, `bidl_amount`, `bidm_amount`, `time_arrival`, `createdon`) VALUES
+(9, 16, 10, 11, 11.00, 22.00, 33.00, '2019-06-13', '2019-06-17'),
+(10, 17, 17, 18, 12.00, 52.00, 63.00, '2019-06-20', '2019-06-18');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jobs`
+-- Table structure for table `chat`
 --
 
-CREATE TABLE `jobs` (
+CREATE TABLE `chat` (
   `id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  `location` varchar(255) NOT NULL,
-  `cost` decimal(15,2) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
-  `description` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `jobs`
---
-
-INSERT INTO `jobs` (`id`, `customer_id`, `location`, `cost`, `start_date`, `end_date`, `description`) VALUES
-(13, 19, 'Hamilton', '1500.00', '2019-06-13', '2019-06-20', 'Wall Painting');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `messages`
---
-
-CREATE TABLE `messages` (
-  `id` int(11) NOT NULL,
-  `job_id` int(11) NOT NULL,
-  `sender` int(11) NOT NULL,
-  `receiver` int(11) NOT NULL,
+  `requierment_id` int(11) NOT NULL,
+  `sendby` int(11) NOT NULL,
+  `receiveby` int(11) NOT NULL,
   `message` text NOT NULL,
-  `date_added` datetime NOT NULL
+  `createdon` date NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `messages`
+-- Dumping data for table `chat`
 --
 
-INSERT INTO `messages` (`id`, `job_id`, `sender`, `receiver`, `message`, `date_added`) VALUES
-(27, 7, 6, 7, 'jhjhjhjh', '2019-06-04 02:12:54'),
-(28, 7, 6, 7, 'this is testing ma', '2019-06-04 02:13:00'),
-(29, 7, 6, 7, 'okay so its working now', '2019-06-04 02:13:05'),
-(30, 7, 6, 7, 'helo ji', '2019-06-04 02:13:14'),
-(31, 7, 6, 7, '', '2019-06-04 02:13:14'),
-(32, 7, 6, 7, 'uuuu', '2019-06-04 02:15:15'),
-(33, 9, 7, 6, 'test', '2019-06-04 04:06:45'),
-(34, 9, 7, 6, '2ndtest', '2019-06-04 04:06:51'),
-(35, 9, 6, 7, '', '2019-06-04 06:06:43');
+INSERT INTO `chat` (`id`, `requierment_id`, `sendby`, `receiveby`, `message`, `createdon`) VALUES
+(20, 16, 11, 10, 'qwqwqw', '2019-06-17'),
+(19, 16, 11, 10, 'ohhh', '2019-06-17'),
+(18, 16, 11, 10, 'hello man', '2019-06-17'),
+(17, 16, 11, 10, '///', '2019-06-17'),
+(15, 16, 11, 10, 'asasasA', '2019-06-17'),
+(16, 16, 11, 10, 'QWQWQ', '2019-06-17'),
+(14, 16, 11, 10, 'asaasa', '2019-06-17'),
+(21, 16, 11, 10, 'last', '2019-06-17'),
+(22, 16, 11, 10, 'sdsada', '2019-06-17'),
+(23, 16, 11, 10, 'q', '2019-06-17'),
+(24, 16, 11, 10, 'bs', '2019-06-17'),
+(25, 16, 11, 10, 'qq', '2019-06-17'),
+(26, 16, 11, 10, 'aaaa', '2019-06-17'),
+(27, 16, 11, 10, '///', '2019-06-17'),
+(28, 16, 11, 10, './/', '2019-06-17'),
+(29, 16, 11, 10, 'hjhjh/', '2019-06-17'),
+(30, 16, 11, 10, 'xcxczxcxc', '2019-06-17'),
+(31, 17, 18, 17, 'hyy', '2019-06-18'),
+(32, 17, 17, 18, 'hy how are you', '2019-06-18');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `member`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `user_type` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
+CREATE TABLE `member` (
+  `id` int(10) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_pass` varchar(50) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `member_type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `member`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `user_type`, `password`) VALUES
-(20, 'krina', 'k@gmail.com', 'trademe', '202cb962ac59075b964b07152d234b70'),
-(21, 'Ami', 'amipatel@gmail.com', 'customer', '0192023a7bbd73250516f069df18b500');
+INSERT INTO `member` (`id`, `user_name`, `user_pass`, `user_email`, `member_type`) VALUES
+(10, 'cmak', 'password', 'cmak@cmak.com', 'member'),
+(11, 'tmak', 'password', 'tmak@tmak.com', 'trader'),
+(12, 'ami', '123', 'patelami1608@gmail.com', 'trader'),
+(13, 'krina', '123', 'krina@mail.com', 'trader'),
+(14, 'ami', '1234', 'ami@gmail.com', 'member'),
+(15, 'akash', '123', 'a@gmail.com', 'member'),
+(16, 'trade', '123', 't@gmail.com', 'trader'),
+(17, 'krinap', 'admin123', 'krinapatel@gmail.com', 'member'),
+(18, 'Amipatel', 'admin123', 'Amipatel@gmail.com', 'trader');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requierments`
+--
+
+CREATE TABLE `requierments` (
+  `requierment_id` int(11) NOT NULL,
+  `member_id` int(10) NOT NULL,
+  `budget` decimal(15,2) NOT NULL,
+  `place_job` varchar(100) NOT NULL,
+  `date_s` date NOT NULL,
+  `date_e` date NOT NULL,
+  `details` text NOT NULL,
+  `create_date` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `requierments`
+--
+
+INSERT INTO `requierments` (`requierment_id`, `member_id`, `budget`, `place_job`, `date_s`, `date_e`, `details`, `create_date`) VALUES
+(16, 10, '2500.00', 'chandigarh', '2019-06-20', '2019-06-08', 'This is testing', '2019-06-17'),
+(15, 15, '4000.00', 'Hamilton', '2019-05-27', '2019-06-17', 'construction', '2019-06-17'),
+(14, 15, '3000.00', 'wellington', '2019-06-05', '2019-06-12', 'painter', '2019-06-17'),
+(13, 15, '2000.00', 'auckland', '2019-06-05', '2019-06-19', 'hii', '2019-06-17'),
+(12, 15, '123.00', 'hamilton', '2019-06-12', '2019-06-26', 'hello', '2019-06-17'),
+(17, 17, '25.00', 'hemiltom', '2019-06-07', '2019-06-14', 'need developer', '2019-06-18');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `estimate`
+-- Indexes for table `bids`
 --
-ALTER TABLE `estimate`
+ALTER TABLE `bids`
+  ADD PRIMARY KEY (`bid_id`);
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `jobs`
+-- Indexes for table `member`
 --
-ALTER TABLE `jobs`
+ALTER TABLE `member`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `messages`
+-- Indexes for table `requierments`
 --
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `requierments`
+  ADD PRIMARY KEY (`requierment_id`),
+  ADD KEY `member_id` (`member_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `estimate`
+-- AUTO_INCREMENT for table `bids`
 --
-ALTER TABLE `estimate`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+ALTER TABLE `bids`
+  MODIFY `bid_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `jobs`
+-- AUTO_INCREMENT for table `chat`
 --
-ALTER TABLE `jobs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+ALTER TABLE `chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
--- AUTO_INCREMENT for table `messages`
+-- AUTO_INCREMENT for table `member`
 --
-ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+ALTER TABLE `member`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `requierments`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+ALTER TABLE `requierments`
+  MODIFY `requierment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
