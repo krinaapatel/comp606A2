638949ea54be87ac4dc35c2207c0edceba7075cc
AmiPatel1608 <47832727+AmiPatel1608@users.noreply.github.com>
2019-06-19 16:34:44 +1200

    updated home.php
    changed layout and added class main

--- home.php
+++ home.php
@@ -1,24 +1,54 @@
-<!DOCTYPE html>
 <html>
-	<head>
-		<title>Registration</title>
-		<link rel="stylesheet" href="css/menu1.css">
-	</head>
-	<body>
-		<div class="wrapper">
+<head lang="en">
+    <meta charset="UTF-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link type="text/css" rel="stylesheet" href="bootstrap-3.2.0-dist\css\bootstrap.css">
+	<link type="text/css" rel="stylesheet" href="main.css">
+    <title>Login</title>
+</head>
+<style>
+.caret {
+    display: none !important;
+}
 
-		<h1>Job Search</h1>
-		<label for="show-menu" class="show-menu">Show Menu</label>
-		<input type="checkbox" id="show-menu">
-		<ul class="navigation">
-			<li><a href="home.php">Home</a></li>
-			<li><a href="jobs.php">View All Jobs</a></li>
-			<li><a href="login.php">Login</a></li>
 
-		</ul>
+.login-panel {
+    margin-top: 53px;
+}
 
-<div class="col-md-12">
-		<img class="banimg" src="images/banner.jpg" height= '400' width='100%'/>
-			</div>
-	</body>
-</html>
+.mybody{
+	padding: 0px !important;
+}
+</style>
+
+<body class="mybody">
+<div id="topheader" class="col-sm-12">
+
+<div class="forsitelogo col-sm-3">
+<a style="display: none;" href="home.php"><img class="imglogo" src="images/logo.png"/></a>
+</div>
+
+<div class="formenu col-sm-7">
+<a href="home.php" class="mymenu">Home</a>
+<?php if(!isset($_SESSION['email'])){ ?>
+<a href="registration.php" class="mymenu">Create Account</a>
+<a href="login.php" class="mymenu">Login</a>
+<?php } ?>
+</div>
+<?php if(isset($_SESSION['email'])){ ?>
+<div class="dropdown col-sm-2">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">My Account
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+      <li><a href="welcome.php">Dashboard</a></li>
+	  <?php if(isset($_SESSION['member_type']) && $_SESSION['member_type']=='member') {?>
+      <li><a href="requierment.php">Create job</a></li>
+	  <?php } ?>
+      <li><a href="logout.php">Logout</a></li>
+    </ul>
+  </div>
+<?php } ?>
+</div>
